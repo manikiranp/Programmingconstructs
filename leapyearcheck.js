28f6339cc0ca7df5c894c0ebdf5f3324e8e2f8c3
manikiranp <manikiranp3@gmail.com>
2020-12-01 20:40:52 +0530

    Check for a leap year condition

---
+++ leapyearcheck.js
@@ -0,0 +1,15 @@
+
+var inpyear = Math.floor((Math.random()*9000))+1000;
+console.log("Year "+inpyear);
+if(inpyear%400==0) {
+	console.log(inpyear+" is leap year");
+}
+else if (inpyear%100==0) {
+	console.log(inpyear+" not a leap year");
+}
+else if (inpyear%4==0) {
+	console.log(inpyear+" is leap year");
+} else {
+	console.log(inpyear+" not a leap year");
+}
+
